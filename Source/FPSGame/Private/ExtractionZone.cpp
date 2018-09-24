5bd7563a69a83d31288ff97627ed26de63166961
Rudra-Trivedi <trivedirudra2@gmail.com>
2018-09-24 17:52:32 +0530

    Extraction zone and UI widget added

---
+++ Source/FPSGame/Private/ExtractionZone.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"	
+#include "FPSCharacter.h"
+#include "FPSGameMode.h" 
+#include "Engine/World.h"
+
+
+// Sets default values
+AExtractionZone::AExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+	DecalComp->DecalSize = FVector(200.0f);
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = OverlapComp;
+
+	OverlapComp->SetHiddenInGame(false);
+
+	DecalComp->SetupAttachment(RootComponent);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AExtractionZone::HandleOverlap);
+}
+
+// Called when the game starts or when spawned
+void AExtractionZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 BodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlap occured"));
+
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);	
+
+
+	if (MyPawn && MyPawn->bIsCarryingObjective)
+	{
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+
+		if (GM)
+		{
+			GM->CompleteMission(MyPawn);
+		}
+
+	}
+
+}
+
+// Called every frame
+void AExtractionZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
